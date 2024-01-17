b22687db105fcd489d768f43043d760a639f6e44
VolatileX1 <128452078+VolatileX1@users.noreply.github.com>
2024-01-17 21:18:09 +0530

    Create fetch_wallpaper.py

---
+++ fetch_wallpaper.py
@@ -0,0 +1,23 @@
+import requests
+import os
+
+# Fetch the Unsplash API access key from GitHub Secrets
+access_key = os.getenv('UNSPLASH_ACCESS_KEY')
+
+if not access_key:
+    raise ValueError('UNSPLASH_ACCESS_KEY not set in environment variables.')
+
+url = f'https://api.unsplash.com/photos/random?client_id={access_key}'
+
+response = requests.get(url)
+data = response.json()
+image_url = data['urls']['full']
+
+# Download the image
+image_response = requests.get(image_url)
+with open('wallpaper.jpg', 'wb') as f:
+    f.write(image_response.content)
+
+# Move the downloaded image to the desired folder
+os.makedirs('wallpapers', exist_ok=True)
+os.rename('wallpaper.jpg', 'wallpapers/wallpaper.jpg')
